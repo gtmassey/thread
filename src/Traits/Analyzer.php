3bfdbed938779a82c2e879a41a339c789ebb42d8
gtmassey <contact@garrettmassey.net>
2023-12-14 16:30:08 -0700

    Update to the Analyzer trait and tests

--- src/Traits/Analyzer.php
+++ src/Traits/Analyzer.php
@@ -15,16 +15,20 @@ trait Analyzer
     }
 
     /**
-     * Returns the most frequent character of the string
+     * Returns the most frequent character of the string,
+     * excluding whitespace.
+     * If two or more characters have the same frequency,
+     * the first one encountered will be returned
      *
      * @return string
      */
     public function mostFrequentCharacter(): string
     {
+        $string = preg_replace('/\s+/', '', $this->string);
         $frequency = [];
-        $strLength = strlen($this->string);
+        $strLength = strlen($string);
         for ($i = 0; $i < $strLength; $i++) {
-            $char = $this->string[$i];
+            $char = $string[$i];
             if (! isset($frequency[$char])) {
                 $frequency[$char] = 0;
             }
@@ -38,29 +42,85 @@ trait Analyzer
     }
 
     /**
+     * Returns the top n most frequent characters in a string,
+     * along with their count. excluding whitespace
+     * default n = 3 if not provided
+     *
      * @param  int  $n
      * @return array<string, int>
      */
-    public function mostFrequentCharacters(int $n = 1): array
+    public function mostFrequentCharacters(int $n = 3): array
     {
-        return [];
+        $string = preg_replace('/\s+/', '', $this->string);
+        $frequency = [];
+        $strLength = strlen($string);
+        for ($i = 0; $i < $strLength; $i++) {
+            $char = $string[$i];
+            if (! isset($frequency[$char])) {
+                $frequency[$char] = 0;
+            }
+            $frequency[$char]++;
+        }
+
+        arsort($frequency);
+
+        return array_slice($frequency, 0, $n);
     }
 
     /**
+     * returns the most frequent word in a string,
+     * where a 'word' is considered a substring
+     * separated by a space character or other whitespace
+     * on both sides
+     *
      * @return string
      */
     public function mostFrequentWord(): string
     {
-        return '';
+        //TODO: check to make sure this works for
+        //      situations where all words
+        //      appear equally frequently
+        $words = $this->splitOnWords();
+        $frequency = [];
+        foreach ($words as $word) {
+            if (! isset($frequency[$word])) {
+                $frequency[$word] = 0;
+            }
+            $frequency[$word]++;
+        }
+
+        $maxCount = max($frequency);
+
+        return array_search($maxCount, $frequency);
     }
 
     /**
+     * returns the top n most frequent words in a string,
+     * where a 'word' is considered a substring
+     * separated by a space character or other whitespace
+     * on both sides
+     * default n = 3 if not provided
+     *
      * @param  int  $n
      * @return array<string, int>
      */
-    public function mostFrequentWords(int $n = 1): array
+    public function mostFrequentWords(int $n = 3): array
     {
-        return [];
+        //TODO: check to make sure this works for
+        //      situations where all words
+        //      appear equally frequently
+        $words = $this->splitOnWords();
+        $frequency = [];
+        foreach ($words as $word) {
+            if (! isset($frequency[$word])) {
+                $frequency[$word] = 0;
+            }
+            $frequency[$word]++;
+        }
+
+        arsort($frequency);
+
+        return array_slice($frequency, 0, $n);
     }
 
     /**
@@ -71,6 +131,16 @@ trait Analyzer
      */
     public function wordCount(): int
     {
-        return count($this->splitOnWords());
+        //TODO: need to first check if the string
+        //      is only whitespace, because exploding
+        //      on whitespace only returns an array of empty
+        //      strings, which defeats the purpose
+        //
+        //TODO: then, need to check if the split
+        //      string array is empty, because if it is
+        //      then the word count is 0
+        return $this->length() === 0
+            ? 0
+            : count($this->splitOnWords());
     }
 }
---
+++ tests/AnalyzerTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Gtmassey\Thread\Tests;
+
+use Gtmassey\Thread\Thread;
+use PHPUnit\Framework\TestCase;
+
+class AnalyzerTest extends TestCase
+{
+
+    public function testLength(): void
+    {
+        $twine = new Thread('One');
+        $this->assertEquals(3, $twine->length());
+
+        $twine = new Thread('Two');
+        $this->assertEquals(3, $twine->length());
+
+        $twine = new Thread('Three');
+        $this->assertEquals(5, $twine->length());
+    }
+
+    public function testMostFrequentCharacter(): void
+    {
+        $twine = new Thread('Hello, world!');
+        $this->assertEquals('l', $twine->mostFrequentCharacter());
+
+        $twine = new Thread('Foo bar baz');
+        $this->assertEquals('o', $twine->mostFrequentCharacter());
+
+        $twine = new Thread('Foo bar baz Foo bar baz');
+        $this->assertEquals('o', $twine->mostFrequentCharacter());
+    }
+
+    public function testMostFrequentCharacters(): void
+    {
+        //TODO: check test validity
+        $twine = new Thread('one two three four');
+        $this->assertEquals(['o' => 3, 'e' => 3, 't' => 2], $twine->mostFrequentCharacters());
+    }
+
+    public function mostFrequentWord(): void
+    {
+        $twine = new Thread('one two three four one');
+        $this->assertEquals('one', $twine->mostFrequentWord());
+
+        $twine = new Thread('one two three four one two');
+        $this->assertEquals('one', $twine->mostFrequentWord());
+
+        $twine = new Thread('one two three four');
+        $this->assertEquals('', $twine->mostFrequentWord());
+    }
+
+    public function mostFrequentWords(): void
+    {
+        //TODO: implement test
+        $this->assert(true);
+    }
+
+    public function testWordCount(): void
+    {
+        //TODO: implement test
+        $this->assert(true);
+    }
+
+}
